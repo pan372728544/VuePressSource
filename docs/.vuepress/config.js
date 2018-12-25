ccb5706d321433bfcd3c7a3f51fb2f0110b0e2f3
panzhijun <panzhijun@gomeplus.com>
2018-12-25 11:37:41 +0800

    source

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Hello VuePress',  // 设置网站标题
+  description : 'Just playing around', // 网站描述
+
+   themeConfig: {
+      // 导航栏链接
+    nav: require('./nav/zh'),
+    // 导航栏结束
+    sidebar: 'auto'
+
+
+  },
+}
+
+
